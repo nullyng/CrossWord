dac2c6b61f323a48f7672d197d859b61c19f61f2
dohee <dohee121@naver.com>
2019-11-25 14:31:17 +0000

    191125 dh_4

--- crossword_dh.c
+++ crossword_dh.c
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <ctype.h>
 
+void add_blank(int, int, int);
 void set_crmode();
 void set_nodelay_mode(void);
 void tty_mode(int);
@@ -25,21 +26,35 @@ void main(){
 	clear();
 	
 	attron(A_BOLD);
-
-	move(5,5);
-	printw("*--------------------------------------------------------------------------*");
-	move(6,5);
-	printw("|1   |2   |3   |    |4   |5   |6   |    |7   |8   |9   |    |10  |11  |12  |");
-	move(7,5);
-	printw("|    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |");
-	move(8,5);
-	printw("----------------------------------------------------------------------------");
-	move(9,5);
-	printw("|13  |    |    |    |14  |    |    |    |15  |    |    |    |16  |    |    |");
-	move(10,5);
-	printw("|    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |");
-	move(11,5);
-	printw("----------------------------------------------------------------------------");
+//                               6    11   16   21   26   31   36   41   46
+	move(3,5);	printw("*--------------------------------------------*");
+	move(4,5);	printw("|1   |    |2   |    |3   |    |4   |    |5   |"); add_blank(4,31,1);
+	move(5,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(5,31,1);
+	move(6,5);	printw("----------------------------------------------");
+	move(7,5);	printw("|    |    |    |    |6   |    |    |    |    |"); add_blank(7,11,1); add_blank(7,21,1); add_blank(7,41,1);
+	move(8,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(8,11,1); add_blank(8,21,1); add_blank(8,41,1);
+	move(9,5);	printw("----------------------------------------------");
+	move(10,5);	printw("|7   |    |    |    |    |    |8   |    |    |"); add_blank(10,31,1);
+	move(11,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(11,31,1);
+	move(12,5);	printw("----------------------------------------------");
+	move(13,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(13,11,3); add_blank(13,31,1); add_blank(13,41,2);
+	move(14,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(14,11,3); add_blank(14,31,1); add_blank(14,41,2);
+	move(15,5);	printw("----------------------------------------------");
+	move(16,5);	printw("|9   |    |10  |    |    |11  |    |    |12  |"); add_blank(16,26,1);
+	move(17,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(17,26,1);
+	move(18,5);	printw("----------------------------------------------");
+	move(19,5);	printw("|    |    |    |    |13  |    |    |    |    |"); add_blank(19,6,2); add_blank(19,21,1); add_blank(19,31,3);
+	move(20,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(20,6,2); add_blank(20,21,1); add_blank(20,31,3);
+	move(21,5);	printw("----------------------------------------------");
+	move(22,5);	printw("|14  |    |    |    |15  |    |16  |    |    |"); add_blank(22,21,1);
+	move(23,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(23,21,1);
+	move(24,5);	printw("----------------------------------------------");
+	move(25,5);	printw("|    |    |17  |    |    |    |    |    |    |"); add_blank(25,11,1); add_blank(25,31,1); add_blank(25,41,1);
+	move(26,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(26,11,1); add_blank(26,31,1); add_blank(26,41,1);
+	move(27,5);	printw("----------------------------------------------");
+	move(28,5);	printw("|18  |    |    |    |19  |    |    |    |    |"); add_blank(28,21,1);
+	move(29,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(29,21,1);
+	move(30,5);	printw("*--------------------------------------------*");
 
 
 
@@ -59,6 +74,21 @@ void main(){
 		return;
 	}
 }
+
+void add_blank(int x, int y, int n){
+	move(x,y);
+	standout();
+	for(int i = 0; i < n; i++){
+		printw("    ");
+		if(i != n-1){
+			standend();
+			printw("|");
+			standout();
+		}
+	}
+	standend();
+}
+
 
 void set_crmode(){ // canonical mode OFF
 	struct termios ttystate;
