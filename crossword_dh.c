3cfd11a1ca7811e1e33d7af21fe898beb9fe6f69
dohee <dohee121@naver.com>
2019-11-29 14:48:16 +0000

    back 기능 추가

--- crossword_dh.c
+++ crossword_dh.c
@@ -9,10 +9,13 @@
 #include <ctype.h>
 #include <string.h>
 
+void crossword_base();
 void add_blank(int, int, int);
-void select_action();
 void clear_box();
-void add();
+
+void select_action_page();
+void select_across_down_page();
+void add_page(int);
 void add_across(int, int,  char*);
 void add_down(int, int,  char*);
 void delete();
@@ -30,91 +33,96 @@ void main(){
 	initscr();
 	clear();
 
-	//                               6    11   16   21   26   31   36   41   46
-	move(3,5);	printw("*--------------------------------------------*");
-	move(4,5);	printw("|1   |    |2   |    |3   |    |4   |    |5   |"); add_blank(4,31,1);
-	move(5,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(5,31,1);
-	move(6,5);	printw("----------------------------------------------");
-	move(7,5);	printw("|    |    |    |    |6   |    |    |    |    |"); add_blank(7,11,1); add_blank(7,21,1); add_blank(7,41,1);
-	move(8,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(8,11,1); add_blank(8,21,1); add_blank(8,41,1);
-	move(9,5);	printw("----------------------------------------------");
-	move(10,5);	printw("|7   |    |    |    |    |    |8   |    |    |"); add_blank(10,31,1);
-	move(11,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(11,31,1);
-	move(12,5);	printw("----------------------------------------------");
-	move(13,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(13,11,3); add_blank(13,31,1); add_blank(13,41,2);
-	move(14,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(14,11,3); add_blank(14,31,1); add_blank(14,41,2);
-	move(15,5);	printw("----------------------------------------------");
-	move(16,5);	printw("|9   |    |10  |    |    |11  |    |    |12  |"); add_blank(16,26,1);
-	move(17,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(17,26,1);
-	move(18,5);	printw("----------------------------------------------");
-	move(19,5);	printw("|    |    |    |    |13  |    |    |    |    |"); add_blank(19,6,2); add_blank(19,21,1); add_blank(19,31,3);
-	move(20,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(20,6,2); add_blank(20,21,1); add_blank(20,31,3);
-	move(21,5);	printw("----------------------------------------------");
-	move(22,5);	printw("|14  |    |    |    |15  |    |16  |    |    |"); add_blank(22,21,1);
-	move(23,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(23,21,1);
-	move(24,5);	printw("----------------------------------------------");
-	move(25,5);	printw("|    |    |17  |    |    |    |    |    |    |"); add_blank(25,11,1); add_blank(25,31,1); add_blank(25,41,1);
-	move(26,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(26,11,1); add_blank(26,31,1); add_blank(26,41,1);
-	move(27,5);	printw("----------------------------------------------");
-	move(28,5);	printw("|18  |    |    |    |19  |    |    |    |    |"); add_blank(28,21,1);
-	move(29,5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(29,21,1);
-	move(30,5);	printw("*--------------------------------------------*");
+	crossword_base();
+	select_action_page();
 
-	attron(A_BOLD|A_UNDERLINE);
-	move(3,56);	printw("Across");
-	attroff(A_BOLD|A_UNDERLINE);
-	move(5,56);	printw("1. Cook on gridiron");
-	move(6,56);	printw("4. Work with shovel");
-	move(7,56);	printw("6. Belonging to us");
-	move(8,56);	printw("7. Repeated another time");
-	move(9,56);	printw("8. Small insect");
-	move(10,56);	printw("9. 10cent coin");
-	move(11,56);	printw("11. Frozen precipitation");
-	move(12,56);	printw("14. Food tin");
-	move(13,56);	printw("15. Grayish green");
-	move(14,56);	printw("17. Hooting bird");
-	move(15,56);	printw("18. Sticky black substance");
-	move(16,56);	printw("19. A lollipop");
-
-	attron(A_BOLD|A_UNDERLINE);
-	move(3,87);	printw("Down");
-	attroff(A_BOLD|A_UNDERLINE);
-	move(5,87);	printw("1. Protect");
-	move(6,87);	printw("2. Northern lreland army");
-	move(7,87);	printw("3. Not short");
-	move(8,87);	printw("4. Empty liquid from container");
-	move(9,87);	printw("5. Acquire");
-	move(10,87);	printw("10. Not major");
-	move(11,87);	printw("12. Fully of weeds");
-	move(12,87);	printw("13. Tootsie ____");
-	move(13,87);	printw("14. Feline");
-	move(14,87);	printw("16. Sick");
-
-	attron(A_BOLD);
-	move(19,56);	printw("*************************************************************");
-	move(20,56);	printw("*                                                           *");
-	move(21,56);	printw("*                                                           *");
-	move(22,56);	printw("*                                                           *");
-	move(23,56);	printw("*                                                           *");
-	move(24,56);	printw("*                                                           *");
-	move(25,56);	printw("*                                                           *");
-	move(26,56);	printw("*                                                           *");
-	move(27,56);	printw("*                                                           *");
-	move(28,56);	printw("*                                                           *");
-	move(29,56);	printw("*                                                           *");
-	move(30,56);	printw("*************************************************************");
-	attroff(A_BOLD);
-
-	select_action();
-
-	move(LINES-1, COLS-1);
-	refresh();
+	
 
 	if(getchar()){
 		tty_mode(1);
 		endwin();
 		return;
 	}
+}
+
+void crossword_base() {
+	//                               6    11   16   21   26   31   36   41   46
+	move(3, 5);	printw("*--------------------------------------------*");
+	move(4, 5);	printw("|1   |    |2   |    |3   |    |4   |    |5   |"); add_blank(4, 31, 1);
+	move(5, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(5, 31, 1);
+	move(6, 5);	printw("----------------------------------------------");
+	move(7, 5);	printw("|    |    |    |    |6   |    |    |    |    |"); add_blank(7, 11, 1); add_blank(7, 21, 1); add_blank(7, 41, 1);
+	move(8, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(8, 11, 1); add_blank(8, 21, 1); add_blank(8, 41, 1);
+	move(9, 5);	printw("----------------------------------------------");
+	move(10, 5);	printw("|7   |    |    |    |    |    |8   |    |    |"); add_blank(10, 31, 1);
+	move(11, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(11, 31, 1);
+	move(12, 5);	printw("----------------------------------------------");
+	move(13, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(13, 11, 3); add_blank(13, 31, 1); add_blank(13, 41, 2);
+	move(14, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(14, 11, 3); add_blank(14, 31, 1); add_blank(14, 41, 2);
+	move(15, 5);	printw("----------------------------------------------");
+	move(16, 5);	printw("|9   |    |10  |    |    |11  |    |    |12  |"); add_blank(16, 26, 1);
+	move(17, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(17, 26, 1);
+	move(18, 5);	printw("----------------------------------------------");
+	move(19, 5);	printw("|    |    |    |    |13  |    |    |    |    |"); add_blank(19, 6, 2); add_blank(19, 21, 1); add_blank(19, 31, 3);
+	move(20, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(20, 6, 2); add_blank(20, 21, 1); add_blank(20, 31, 3);
+	move(21, 5);	printw("----------------------------------------------");
+	move(22, 5);	printw("|14  |    |    |    |15  |    |16  |    |    |"); add_blank(22, 21, 1);
+	move(23, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(23, 21, 1);
+	move(24, 5);	printw("----------------------------------------------");
+	move(25, 5);	printw("|    |    |17  |    |    |    |    |    |    |"); add_blank(25, 11, 1); add_blank(25, 31, 1); add_blank(25, 41, 1);
+	move(26, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(26, 11, 1); add_blank(26, 31, 1); add_blank(26, 41, 1);
+	move(27, 5);	printw("----------------------------------------------");
+	move(28, 5);	printw("|18  |    |    |    |19  |    |    |    |    |"); add_blank(28, 21, 1);
+	move(29, 5);	printw("|    |    |    |    |    |    |    |    |    |"); add_blank(29, 21, 1);
+	move(30, 5);	printw("*--------------------------------------------*");
+
+	attron(A_BOLD | A_UNDERLINE);
+	move(3, 56);	printw("Across");
+	attroff(A_BOLD | A_UNDERLINE);
+	move(5, 56);	printw("1. Cook on gridiron");
+	move(6, 56);	printw("4. Work with shovel");
+	move(7, 56);	printw("6. Belonging to us");
+	move(8, 56);	printw("7. Repeated another time");
+	move(9, 56);	printw("8. Small insect");
+	move(10, 56);	printw("9. 10cent coin");
+	move(11, 56);	printw("11. Frozen precipitation");
+	move(12, 56);	printw("14. Food tin");
+	move(13, 56);	printw("15. Grayish green");
+	move(14, 56);	printw("17. Hooting bird");
+	move(15, 56);	printw("18. Sticky black substance");
+	move(16, 56);	printw("19. A lollipop");
+
+	attron(A_BOLD | A_UNDERLINE);
+	move(3, 87);	printw("Down");
+	attroff(A_BOLD | A_UNDERLINE);
+	move(5, 87);	printw("1. Protect");
+	move(6, 87);	printw("2. Northern lreland army");
+	move(7, 87);	printw("3. Not short");
+	move(8, 87);	printw("4. Empty liquid from container");
+	move(9, 87);	printw("5. Acquire");
+	move(10, 87);	printw("10. Not major");
+	move(11, 87);	printw("12. Fully of weeds");
+	move(12, 87);	printw("13. Tootsie ____");
+	move(13, 87);	printw("14. Feline");
+	move(14, 87);	printw("16. Sick");
+
+	attron(A_BOLD);
+	move(19, 56);	printw("*************************************************************");
+	move(20, 56);	printw("*                                                           *");
+	move(21, 56);	printw("*                                                           *");
+	move(22, 56);	printw("*                                                           *");
+	move(23, 56);	printw("*                                                           *");
+	move(24, 56);	printw("*                                                           *");
+	move(25, 56);	printw("*                                                           *");
+	move(26, 56);	printw("*                                                           *");
+	move(27, 56);	printw("*                                                           *");
+	move(28, 56);	printw("*                                                           *");
+	move(29, 56);	printw("*                                                           *");
+	move(30, 56);	printw("*************************************************************");
+	attroff(A_BOLD);
+
+	move(LINES - 1, COLS - 1);
+	refresh();
 }
 
 void add_blank(int x, int y, int n){
@@ -131,7 +139,7 @@ void add_blank(int x, int y, int n){
 	standend();
 }
 
-void select_action(){
+void select_action_page(){
 	int key;
 	int selection = 1;
 	char *selections[] = {"[1] Add",
@@ -165,7 +173,7 @@ void select_action(){
 			break;
 	}
 
-	if(selection == 1) add();
+	if(selection == 1) select_across_down_page();
 	else if(selection == 2) delete();
 	else if(selection == 3) submit();
 	else exit_puzzle();
@@ -180,48 +188,54 @@ void clear_box(){
 	refresh();
 }
 
-
-void add(){
+void select_across_down_page() {
 	int i;
 	int key;
 	int selection = 1; // 1:Across, 2:Down
-	char *selections[] = {"[1] Across", "[2] Down"};
+	char *selections[] = { "[1] Across", "[2] Down" };
+
+	clear_box();
+
+	keypad(stdscr, TRUE);
+	while (1) {
+		for (i = 0; i < 2; i++) {
+			move(24 + i, 80);
+			if (i + 1 == selection)
+				attron(A_REVERSE);
+			printw("%s", selections[i]);
+			attroff(A_REVERSE);
+			move(LINES - 1, COLS - 1);
+			refresh();
+		}
+
+		switch (key = getch()) {
+		case KEY_DOWN:
+			if (++selection == 3) selection = 2;
+			break;
+		case KEY_UP:
+			if (--selection == 0) selection = 1;
+			break;
+		default:
+			break;
+		}
+		if (key == 10)
+			break;
+	}
+
+	if (selection == 1) add_page(1);
+	else add_page(2);
+}
+
+void add_page(int selection){
+	int i;
 	char *across[] = {"", "grill", "", "", "dig", "",  "our", "again", "ant", "dime", "",  "snow", "", "",  "can", "olive", "", "owl", "tar", "lolly"};
 	char *down[] = {"", "guard", "IRA", "long", "drain", "get", "", "", "", "",  "minor", "", "weedy", "roll", "cat", "", "ill"};
 	char input[20];
 	int number; // Across, Down의 몇 번째 단어인가?
 
-	clear_box();	
-
-	keypad(stdscr, TRUE);
-	while(1){
-		for(i = 0; i < 2; i++){
-			move(24+i,80);
-			if(i + 1 == selection)
-				attron(A_REVERSE);
-			printw("%s", selections[i]);
-			attroff(A_REVERSE);
-			move(LINES-1, COLS-1);
-			refresh();
-		}
-
-		switch(key = getch()){
-			case KEY_DOWN:
-				if(++selection == 3) selection = 2;
-				break;
-			case KEY_UP:
-				if(--selection == 0) selection = 1;
-				break;
-			default:
-				break;
-		}
-		if(key == 10)
-			break;
-	}
-
 	while(1){
 		clear_box();
-		move(20,58); printw("Please write the answer. (example: 20 table)");
+		move(20,58); printw("Please write the answer. (example: 20 table || back)");
 		move(21,58); printw(": ");
 		refresh();
 		
@@ -236,7 +250,10 @@ void add(){
 		}
 		clear_box();
 
-		move(20,58); printw("%s", input);
+		if (strcmp(input, "back") == 0) {
+			select_across_down_page();
+			return;
+		}
 
 		number = atoi(input);
 		char *pass; // input에서 word만을 뗀 것
@@ -278,7 +295,6 @@ void add(){
 				sleep(1);
 			}
 		}
-		
 	}
 }
 
